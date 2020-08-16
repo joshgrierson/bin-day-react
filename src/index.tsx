ead8844ca834df1554492f37807b28a9a2632ef3
Joshua Grierson <jgrierson101@gmail.com>
2020-08-16 16:13:24 +0100

    [app] - refactored

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-import './index.css';
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
